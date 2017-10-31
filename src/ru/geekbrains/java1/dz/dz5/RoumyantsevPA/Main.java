584587f8730e7ca9c1a305f7f20a4f5682a7e8a6
knifffe <knifffe@yandex.ru>
2017-10-31 15:23:04 +0300

    создал класс человек, и рандомное его создание

---
+++ src/ru/geekbrains/java1/dz/dz5/RoumyantsevPA/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.java1.dz.dz5.RoumyantsevPA;
+
+public class Main {
+    public static void main(String[] args) {
+        Human me=new Human();
+        me.printHuman();
+
+    }
+}
