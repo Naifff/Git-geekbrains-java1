7c83021a20e429ea3e56e7b731781f4e97e121c3
Naif <knifffe@yandex.ru>
2017-11-04 11:10:12 +0300

    создал генератор животных

--- src/ru/geekbrains/java1/dz/dz6/RoumyantsevPA/Generator.java
+++ src/ru/geekbrains/java1/dz/dz6/RoumyantsevPA/Generator.java
@@ -1,4 +1,28 @@
 package ru.geekbrains.java1.dz.dz6.RoumyantsevPA;
 
+import java.util.Random;
+
 public class Generator {
+
+    public static Animal gen(int i){
+        Random rnd=new Random();
+        Animal animal;
+       switch (i){
+           case 0:
+                animal=new Cat();
+               break;
+           default:
+               animal=new Dog();
+               break;
+       }
+        String[] names = {"Яшка", "Яша", "Ярофей", "Ягуар", "Яндекс", "Ярик", "Яков", "Якоб", "Ямб", "Яцек", "Яволь", "Януш", "Янчик", "Яго", "Ярис", "Яхонт", "Ярый", "Ярило", "Ярополк"};
+        String[] colors = {"темно-красный", "кошениль", "алый", "ализариновый", "малиновый", "кардинал", "маджента", "баклажан", "глициния", "лимонный", "тауп", "нефритовый", "аквамарин", "индиго", "берлинская лазурь", "гридеперлевый", "антрацитовый", "маренго"};
+        int age;
+        animal.setAge(rnd.nextInt(20)+1);
+        animal.setColor(colors[rnd.nextInt(colors.length)]);
+        animal.setName(names[rnd.nextInt((names.length))]);
+        return animal;
+    }
+
+
 }
--- src/ru/geekbrains/java1/dz/dz6/RoumyantsevPA/Mmain.java
+++ src/ru/geekbrains/java1/dz/dz6/RoumyantsevPA/Mmain.java
@@ -2,29 +2,29 @@ package ru.geekbrains.java1.dz.dz6.RoumyantsevPA;
 
 import java.util.Random;
 
+import static ru.geekbrains.java1.dz.dz6.RoumyantsevPA.Generator.gen;
+
 public class Mmain {
     public static void main(String[] args) {
 
 
         Animal animals[] = new Animal[10];
         //int count = 0;
-        String[] names = {"Яшка", "Яша", "Ярофей", "Ягуар", "Яндекс", "Ярик", "Яков", "Якоб", "Ямб", "Яцек", "Яволь", "Януш", "Янчик", "Яго", "Ярис", "Яхонт", "Ярый", "Ярило", "Ярополк"};
-        String[] colors = {"темно-красный", "кошениль", "алый", "ализариновый", "малиновый", "кардинал", "маджента", "баклажан", "глициния", "лимонный", "тауп", "нефритовый", "аквамарин", "индиго", "берлинская лазурь", "гридеперлевый", "антрацитовый", "маренго"};
-        int age;
+//        String[] names = {"Яшка", "Яша", "Ярофей", "Ягуар", "Яндекс", "Ярик", "Яков", "Якоб", "Ямб", "Яцек", "Яволь", "Януш", "Янчик", "Яго", "Ярис", "Яхонт", "Ярый", "Ярило", "Ярополк"};
+//        String[] colors = {"темно-красный", "кошениль", "алый", "ализариновый", "малиновый", "кардинал", "маджента", "баклажан", "глициния", "лимонный", "тауп", "нефритовый", "аквамарин", "индиго", "берлинская лазурь", "гридеперлевый", "антрацитовый", "маренго"};
+//        int age;
         Random rnd = new Random();
         for (int i = 0; i < animals.length; i++) {
-            if (i % 2 == 0) {
-                animals[i] = new Cat(names[rnd.nextInt(names.length)], colors[rnd.nextInt(colors.length)], rnd.nextInt(20));
-            } else {
-                animals[i] = new Dog(names[rnd.nextInt(names.length)], colors[rnd.nextInt(colors.length)], rnd.nextInt(20));
-            }
+
+                animals[i] = gen(i%2);
+
         }
         for (Animal animal : animals) {
 
 
             animal.printAnimal();
-            animal.run(rnd.nextInt(1000));
-            animal.jump((((double)rnd.nextInt(400))/100));
+            animal.run(rnd.nextInt(500));
+            animal.jump((((double)rnd.nextInt(200))/100));
             animal.swim(rnd.nextInt(20));
             System.out.println("\n");
         }
