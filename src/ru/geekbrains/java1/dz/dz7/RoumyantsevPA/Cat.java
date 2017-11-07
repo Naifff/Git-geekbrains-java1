dbf7ab6333c4e1679a8be5e8e04295f121b62f4e
knifffe <knifffe@yandex.ru>
2017-11-07 09:23:48 +0300

    дз (reverted from commit d6e55c537d1281db67db842ef4250d88e73cb612) (reverted from commit 456febc47edff05e453b08b93c554e0c403f0dee)

--- src/ru/geekbrains/java1/dz/dz7/RoumyantsevPA/Cat.java
+++ src/ru/geekbrains/java1/dz/dz7/RoumyantsevPA/Cat.java
@@ -1,4 +1,13 @@
 package ru.geekbrains.java1.dz.dz7.RoumyantsevPA;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+    public void eat(Plate p) {
+        p.decreaseFood(appetite);
+    }
 }
