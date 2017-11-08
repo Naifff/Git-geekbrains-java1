1da40eeef7596a8f9e43fde134e7599e7e766c50
knifffe <knifffe@yandex.ru>
2017-11-08 16:19:37 +0300

    окна ХО

--- src/ru/geekbrains/java1/dz/dz8/RoumyantsevPA/Window.java
+++ src/ru/geekbrains/java1/dz/dz8/RoumyantsevPA/Window.java
@@ -1,13 +1,28 @@
 package ru.geekbrains.java1.dz.dz8.RoumyantsevPA;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Window extends JFrame {
 
     public Window(){
-        setTitle("Test Window");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(300,300,400,400);
-        setVisible(true);
+        int cols=3;
+        setBounds ( 0 , 0 , cols*100 , cols*100 );
+
+        setDefaultCloseOperation ( WindowConstants . EXIT_ON_CLOSE );
+        setResizable(false);
+
+        setTitle ( cols +" в ряд" );
+        JButton [] jbs = new JButton [ cols*cols];
+        setLayout ( new GridLayout ( cols , cols ));
+        for ( int i = 0 ; i < jbs . length ; i ++) {
+            jbs [ i ] = new JButton ( "#" + i );
+            add ( jbs [ i ]);
+        }
+        setVisible ( true );
     }
 }
