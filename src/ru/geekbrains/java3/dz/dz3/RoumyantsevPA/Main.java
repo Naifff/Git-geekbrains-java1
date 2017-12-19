f5516eb9bb7d236a790c5d37f57530fc37672a53
Naif <knifffe@yandex.ru>
2017-12-20 00:26:32 +0300

    дз 3 все

--- src/ru/geekbrains/java3/dz/dz3/RoumyantsevPA/Main.java
+++ src/ru/geekbrains/java3/dz/dz3/RoumyantsevPA/Main.java
@@ -15,45 +15,48 @@ Enumeration<InputStream> e = Collections.enumeration(al);
  */
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        long t = System.currentTimeMillis();
         String filename = ".\\src\\ru\\geekbrains\\java3\\dz\\dz3\\RoumyantsevPA\\ex1.txt";
         int size = 50;
         String path = ".\\src\\ru\\geekbrains\\java3\\dz\\dz3\\RoumyantsevPA\\";
-        optimizedArrayOutToFile(50, filename);
-        readFileWithByteArrayEx(50, filename);
-        sequenceInputEx(5,100,path);
-
+        fileCreator(50, filename);
+        fileReader(filename);
+        merge(5, 100, path);
+        System.out.println("1 и 2 задание" + (System.currentTimeMillis() - t) + "мс.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Введите номер страницы:");
+        int l = sc.nextInt();
+        t = System.currentTimeMillis();
+        readList(l,path);
+        System.out.println("3 задание" + (System.currentTimeMillis() - t) + "мс.");
 
     }
 
-    private static void optimizedArrayOutToFile(int size, String filename) {
+    private static void fileCreator(int size, String filename) {
         long t = System.currentTimeMillis();
         Random rnd = new Random();
         try (FileOutputStream out = new FileOutputStream(filename);) {
 
             byte[] arr = new byte[size];
             for (int i = 0; i < arr.length; i++) {
-                arr[i] = (byte) ((byte) rnd.nextInt() * 254);
+                arr[i] = (byte) randLetter();
             }
-//            for (int i = 0; i < 1024 * 5; i++) {
             out.write(arr);
-//            }
             System.out.println("время гененации файла:" + (System.currentTimeMillis() - t) + "мс.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void readFileWithByteArrayEx(int size, String filename) {
+    private static void fileReader(String filename) {
         long t = System.currentTimeMillis();
         try (FileInputStream in = new FileInputStream(filename);) {
-            byte[] arr = new byte[size];
+            byte[] arr = new byte[50];
             int n;
             while ((n = in.read(arr)) > 0) {
                 System.out.print(new String(arr, 0, n));
@@ -65,19 +68,15 @@ public class Main {
         }
     }
 
-    private static void sequenceInputEx(int files, int size, String path) {
+    private static void merge(int files, int size, String path) {
+        System.out.println();
+
         SequenceInputStream in = null;
         try {
-//            in = new SequenceInputStream(Collections.enumeration(new ArrayList<InputStream>(Arrays.asList(
-//                    new FileInputStream("1.txt"),
-//                    new FileInputStream("2.txt"),
-//                    new FileInputStream("3.txt"),
-//                    new FileInputStream("4.txt")
-//            ))));
             ArrayList<InputStream> ipsarr = new ArrayList<>();
 
             for (int i = 0; i < files; i++) {
-                optimizedArrayOutToFile(size, path + "Ex2" + i + ".txt");
+                fileCreator(size, path + "Ex2" + i + ".txt");
                 ipsarr.add(new FileInputStream(path + "Ex2" + i + ".txt"));
             }
             in = new SequenceInputStream(Collections.enumeration(ipsarr));
@@ -86,18 +85,19 @@ public class Main {
                 byte[] arr = new byte[size];
                 int x;
                 int i = 0;
+                long t = System.currentTimeMillis();
                 while ((x = in.read()) != -1) {
-                    if (i > size) {
+                    if (i >= size) {
                         i = 0;
                         out.write(arr);
                     }
-                    System.out.println(i);
+                    // System.out.println(i);
                     arr[i] = (byte) x;
                     i++;
                 }
                 out.write(arr);
 
-
+                System.out.println("время склейки " + files + " файлов по " + size + " байт:" + (System.currentTimeMillis() - t) + "мс.");
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -110,5 +110,48 @@ public class Main {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void readList(int l, String path) {
+        long t = System.currentTimeMillis();
+        try (FileOutputStream out = new FileOutputStream(path+"BIG.txt");) {
+
+            byte[] arr = new byte[1024];
+            for (int i = 0; i < 1024; i++) {
+
+                arr[i] = (byte) randLetter();
+            }
+            for (int i = 0; i < 1024 * 10; i++) {
+                out.write(arr);
+            }
+            System.out.println("время гененации файла в 10MB: " + (System.currentTimeMillis() - t) + "мс.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        t = System.currentTimeMillis();
+        try (RandomAccessFile raf = new RandomAccessFile(path+"BIG.txt", "r")) {
+            int start = l * 1800;
+            char[] arr = new char[1800];
+            for (int i = start; i < start + 1800; i++) {
+                raf.seek(l * 1800 + i);
+                arr[i - start] = (char) raf.read();
+            }
+            System.out.println("чтение страницы: " + (System.currentTimeMillis() - t) + "мс.");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+            System.out.println("вывод в консоль " + (System.currentTimeMillis() - t) + "мс.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static char randLetter() {
+        String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ,.!?";
+        Random rnd = new Random();
+        return str.charAt(rnd.nextInt(str.length()));
+
     }
 }
